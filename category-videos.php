1f057edd78ae47d07a747944a65ea5b3ebc6363a
Ronei Carlos <41250984+RoneiCarlos@users.noreply.github.com>
2020-09-19 13:01:14 -0300

    Update category-videos.php

--- category-videos.php
+++ category-videos.php
@@ -9,41 +9,42 @@ get_header();
 
 
 <div class="container">
-<h1>Vídeos</h1>
-    <div class="row">
+<h1>Eventos</h1>
+
+<div class="row">
         <div class="col-md-8">
+            <div class="row">
+                <?php
+		    $my_args = array(
+                        'post_type' => 'post',
+                    	'category_name' => array(
+				'videos',
+                            	'videos-educomunicacao',
+                            	'videos-educacao-ambiental',
+                            	'entrevistas'
+			),
+		    );
+                    $my_query = new WP_Query ($my_args);
 
-            <?php 
-                $my_args = array(
-                    'post_type' => 'post',
-                    'category_name' => array('videos',
-                                             'videos-educomunicacao',
-                                             'videos-educacao-ambiental',
-                                             'entrevistas'),
-                    'posts_per_page' => '10'
-                );
-			 	
-
-                $my_query = new WP_Query ($my_args);
-            ?>
-            <div class="container row">
-                <?php if(have_posts()):while(have_posts()):the_post(); ?>
-
-                    <div class="card col-md-5 mr-3 mb-3">
-                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid')); ?></a>
-                        <div class="card-body">
-                            <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                            <a href="<?php the_permalink(); ?>" class="btn btn-outline-info">Veja mais</a>
+                    if( $my_query->have_posts()):while(have_posts()):the_post(); ?>
+                                
+                        <div class="col-md-6">
+                            <div class="card mb-3">
+                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-thumbnail', array('class' => 'card-img-top img-fluid')); ?></a>
+                                <div class="card-body">
+									<a href="<?php the_permalink(); ?>"><h5 class="card-title"><?php the_title(); ?></h5></a>
+                                    <a href="<?php the_permalink(); ?>"><button class="btn btn-outline-info">Veja o post</button></a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-
-                <?php endwhile;endif;wp_reset_query(); ?> 
+                <?php endwhile;endif;wp_reset_query(); ?>
             </div>
             <div class="pagination col-md-5 mb-4">
                 <?php previous_posts_link('< Mais recentes'); ?> <?php next_posts_link('Mais antigos >'); ?>
-            </div>       
+            </div>
         </div>
         <?php get_sidebar(); ?>
     </div>
 </div>
+
 <?php get_footer(); ?>
